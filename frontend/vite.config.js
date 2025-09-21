6ec5b866bccb0d0dfb0c932e46bac066503af22f
er-aryan <aryansharma200203@email.com>
2025-09-21 12:13:58 -0700

    test1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Portfolio/',  // 👈 IMPORTANT: must match your repo name
-})
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return defineConfig({
+    plugins: [react()],
+    base: env.VITE_APP_BASE || '/',
+  })
+}
